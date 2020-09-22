34f0ed2b80be9f0690af43675e66cddee25d1149
Shafah <shafahummer11@gmail.com>
2020-09-22 17:22:58 +0530

    added new thing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const PORT = process.env.PORT || 5000
 const mongoose = require('mongoose')
-const {MOGOURI} = require('./keys')
+const {MOGOURI} = require('./config/keys')
 app.use(express.json())
 
 const authRoute = require('./routes/auth')
