f78598e0abd5bbc90c66dcb7fbdeb9c7bbb03994
Shafah <shafahummer11@gmail.com>
2020-09-24 18:32:57 +0530

    Initial commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const { validationResult } = require('express-validator')
+const bcrypt = require('bcrypt')
 
 
 exports.signup = (req, res) => {
@@ -19,24 +20,69 @@ exports.signup = (req, res) => {
             if (savedUser) {
                 return res.status(422).json({ error: "Email id already registered!" })
             } else {
-                const user = new User(req.body)
 
-                user.save((err, user) => {
-                    if (err) {
+                bcrypt.hash(password, 12, (err, hash) => {
+                    if (hash) {
+                        const user = new User({
+                            email,
+                            password:hash,
+                            name
+                        })
+
+                        user.save((err, user) => {
+                            if (err) {
+                                return res.status(400).json({
+                                    error: "Not able to save user in DB"
+                                })
+                            }
+                            res.json({
+                                message: "Successfully created...",
+                                data: {
+                                    name: user.name,
+                                    email: user.email,
+                                    id: user._id
+                                }
+
+                            })
+                        })
+                    }
+                    else{
                         return res.status(400).json({
                             error: "Not able to save user in DB"
                         })
                     }
-                    res.json({
-                        message: "Successfully created...",
-                        data: {
-                            name: user.name,
-                            email: user.email,
-                            id: user._id
-                        }
+                });
 
-                    })
-                })
+
             }
         })
+}
+
+exports.signin = (req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ error: errors.array()[0].msg });
+    }
+
+    const { email, password } = req.body
+    if (!email || !password) {
+        res.status(400).json({ error: "All fileds are mandatory!" })
+    }
+
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: "User email does not exist!"
+            })
+        }
+        bcrypt.compare(password, user.password, (err, result)=> {
+            // result == true
+            if(result){
+                return res.json({message:"Successfully signed in..."})
+            }
+            else{
+                return res.json({error:"Invalid email or password!"})
+            }
+        });
+    })
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,18 @@
 var express = require('express')
 var router = express.Router()
 const { check } = require('express-validator')
-const {signup} = require('../controllers/auth')
+const {signup,signin} = require('../controllers/auth')
 
 router.post('/signup',[
     check("name").isLength({ min: 3 }).withMessage('name must be at least 3 chars long'),
     check("email").isEmail().withMessage('enter a valid email'),
     check("password").isLength({ min: 5 }).withMessage('password must be at least 5 chars long'),
 ], signup)
+
+router.post('/signin',[
+    check("email").isEmail().withMessage('enter a valid email'),
+    check("password").isLength({ min: 5 }).withMessage('password must be at least 5 chars long'),
+], signin)
 
 router.get('/',(req,res)=>{
     res.send("This is home page")
